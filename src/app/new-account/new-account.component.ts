d5a78949b27ad4fe3fc90ed4ce89f0c2cb175e27
Kim Hyeoncheol <effectmix@gmail.com>
2020-07-31 23:58:34 +0900

    Injecting the Logging Service into Components

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,18 +1,30 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from "../logging.service";
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService],  // Provides simply means we tell Angular how to create the it (LoggingService).
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private loggingService:LoggingService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
   }
+
+  /**
+   * With Providers, Angular when analyzing the component, recognizes that it should be able to give us
+   * such a loggingService and it will set itself up to be able to do so.
+   * and when it then actually builds the component, constructs it, it sees that we want to have such an instance
+   * and it will know how to give us such an instance
+   */
+
 }
